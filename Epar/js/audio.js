f87f7ffe332b6528476ab7b54eb2eae968824329
Carlos Petraglia <149637669+CarlosPtraglia@users.noreply.github.com>
2023-11-13 23:31:25 -0300

    Delete Epar directory

--- Epar/js/audio.js
+++
@@ -1,9 +0,0 @@
-function tocarAudio(tocar) {
-    var audio = document.getElementById("somJogo");
-    var source = audio.querySelector("source");
-
-    audio.pause();
-    source.src = tocar;
-    audio.load();
-    audio.play();
-}
